2a6b27edcaf369be1e6dc9ad5d7f481a844578f9
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-06-23 00:22:20 -0400

    added side nav
    added side nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,11 @@
-import {Component, OnInit} from '@angular/core';
-import {LoadingService} from './loading/loading.service';
-import {MessagesService} from './messages/messages.service';
-import {AuthStore} from './services/auth.store';
-
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements  OnInit {
-
-    constructor(public auth: AuthStore) {
-
-    }
-
-    ngOnInit() {
-
-
-    }
-
-  logout() {
-        this.auth.logout();
-
-  }
-
+export class AppComponent {
+  loading = false;
+  logout() {}
 }
