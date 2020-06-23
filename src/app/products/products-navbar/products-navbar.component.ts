ed56eb92ad37101e4d626d55f4d574c13cf90e63
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-06-23 00:49:11 -0400

    generated the necessary components
    generated the necessary components

---
+++ src/app/products/products-navbar/products-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'products-navbar',
+  templateUrl: './products-navbar.component.html',
+  styleUrls: ['./products-navbar.component.css']
+})
+export class ProductsNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -5,9 +5,13 @@ import { ProductsRoutingModule } from "./products-routing.module";
 import { FormsModule } from "@angular/forms";
 import { ProductsListComponent } from "./product-list/products-list.component";
 import { ProductDetailComponent } from "./product-detail/product-detail.component";
+import { ProductFormComponent } from './product-form/product-form.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { EditProductComponent } from './edit-product/edit-product.component';
+import { ProductsNavbarComponent } from './products-navbar/products-navbar.component';
 
 @NgModule({
-  declarations: [ProductsListComponent, ProductDetailComponent],
+  declarations: [ProductsListComponent, ProductDetailComponent, ProductFormComponent, AddProductComponent, EditProductComponent, ProductsNavbarComponent],
   imports: [CommonModule, FormsModule, ProductsRoutingModule],
 })
 export class ProductsModule {}
